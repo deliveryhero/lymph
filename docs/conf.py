1fbc9356eb1c9581ee4f52ab1d0bb6c01d6df37b
Johannes Dollinger <johannes.dollinger@deliveryhero.com>
2014-06-02 16:53:18 +0200

    Updated documentation copyright

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'iris'
-copyright = u'2013, deliveryhero'
+copyright = u'2013–2014, Delivery Hero Holding GmbH'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
