7cb982051dda9b0a688130e8fa8e2509447eb2a0
Mouad Benchchaoui <mouad.benchchaoui@deliveryhero.com>
2014-11-20 11:49:16 +0100

    BUGFIX: Fix hardcoded help message in 'lymph help'
    Because other libraries can register a new cli entry point, when
    calling 'lymph help' user should be able to see all registered
    cli commands and not just the lymph hardcoded one.

--- lymph/cli/help.py
+++ lymph/cli/help.py
@@ -1,22 +1,47 @@
-from lymph.cli.base import Command, format_docstring, get_command_class
+from lymph.cli.base import Command, get_command_classes, get_command_class, format_docstring
 
 
-HELP = format_docstring("""
-Usage: lymph [options] <command> [<args>...]
+HELP = 'Usage: lymph [options] <command> [<args>...]'
+
+TEMPLATE = HELP + """
 
 {COMMON_OPTIONS}
 
 Commands:
-  instance   Run a single service instance (one process).
-  node       Run a node service that manages a group of processes on the same
-             machine.
-  request    Send a request message to some service and output the reply.
-  inspect    Describe the available rpc methods of a service.
-  tail       Stream the logs of one or more services.
-  discover   Show available services.
-  help       Display help information about lymph.
+%s
+"""
 
-""")
+
+def _format_help(name, description, space=13, min_space=2):
+    r"""Format ``name`` + ``description`` in an unified format
+    that can be used to print beautiful help messages.
+
+    If the name is too long (length is greater than ``space - min_space``
+    than the name and description will appear in different lines.
+
+    Example:
+
+        >>> print(format_help('foo', 'foobar'))
+        foo          foobar
+        >>> print(format_help('foo', 'foobar', space=4))
+        foo
+            foobar
+        >>> print('\n'.join([
+        ...     format_help('help', 'Print help message'),
+        ...     format_help('shell', 'Open an interactive Python shell.')
+        ... ]))
+        ...
+        help         Print help message
+        shell        Open an interactive Python shell.
+
+
+    """
+    if space - len(name) < min_space:
+        return '\n'.join([
+            name, (' ' * space) + description
+        ])
+    else:
+        return name + (' ' * (space - len(name))) + description
 
 
 class HelpCommand(Command):
@@ -26,10 +51,22 @@ class HelpCommand(Command):
 
     short_description = 'Display help information about lymph.'
     needs_config = False
+    _description = None
+
+    @property
+    def description(self):
+        if self._description is None:
+            classes = get_command_classes()
+            cmds = []
+            for name, cls in classes.items():
+                cmds.append('  ' + _format_help(name, cls.short_description))
+            self._description = format_docstring(TEMPLATE % '\n'.join(cmds))
+        return self._description
 
     def run(self):
         name = self.args['<command>']
         if name:
             print(get_command_class(name).get_help())
         else:
-            print(HELP)
+            print(self.description)
+
