16dda42316176f0ad9c747731764855792fe88d6
Johannes Dollinger <johannes.dollinger@deliveryhero.com>
2014-12-22 11:55:04 +0100

    Allow observing more than one action at once

--- lymph/utils/observables.py
+++ lymph/utils/observables.py
@@ -1,11 +1,15 @@
-# Taken from https://github.com/delivero/lymph-storage/blob/master/lymph/storage/observables.py
+
 class Observable(object):
     def __init__(self):
         self.observers = {}
 
     def notify_observers(self, action, *args, **kwargs):
+        kwargs.setdefault('action', action)
         for callback in self.observers.get(action, ()):
             callback(*args, **kwargs)
 
-    def observe(self, action, callback):
-        self.observers.setdefault(action, []).append(callback)
+    def observe(self, actions, callback):
+        if not isinstance(actions, (tuple, list)):
+            actions = (actions,)
+        for action in actions:
+            self.observers.setdefault(action, []).append(callback)
