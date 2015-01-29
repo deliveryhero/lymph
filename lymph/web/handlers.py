237477bc3578e2757ad00b5df04ab5228e3318e8
Dushyant Rijhwani <dushyantrijhwani@gmail.com>
2015-01-29 17:54:42 +0100

    Setting the reader to utf-8 for web handler; supporting py34

--- lymph/web/handlers.py
+++ lymph/web/handlers.py
@@ -1,3 +1,4 @@
+import codecs
 import json
 
 from werkzeug.exceptions import MethodNotAllowed
@@ -7,6 +8,9 @@ http_methods = ('get', 'post', 'head', 'options', 'put', 'delete')
 
 
 class RequestHandler(object):
+
+    stream_reader = codecs.getreader("utf-8")
+
     def __init__(self, interface, request):
         self.request = request
         self.interface = interface
@@ -19,7 +23,7 @@ class RequestHandler(object):
     def json(self):
         # FIXME: should we really keep a reference to the parsed body?
         if self._json is None:
-            self._json = json.load(self.request.stream)
+            self._json = json.load(self.stream_reader(self.request.stream))
         return self._json
 
     def dispatch(self, args):
