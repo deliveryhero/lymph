bf9a0518ebbe21f049ceaf4b3a398a4f03370a18
Johannes Dollinger <emulbreh@googlemail.com>
2015-04-21 22:25:59 +0200

    Preparing 0.3 release

--- lymph/monkey.py
+++ lymph/monkey.py
@@ -18,7 +18,7 @@ patch._initialized = False
 
 def _export():
     import lymph
-    lymph.__version__ = '0.2.1'
+    lymph.__version__ = '0.3.0'
 
     from lymph.exceptions import RpcError, LookupFailure, Timeout
     from lymph.core.decorators import rpc, raw_rpc, event
