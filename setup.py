6e3475fbf348d15d8212e294e5c1fe2fe3120efd
Johannes Dollinger <johannes.dollinger@deliveryhero.com>
2015-06-10 15:44:28 +0200

    Fixed monotime capitalization

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ install_requires = [
 dependency_links = []
 
 if sys.version_info.major == 2:
-    install_requires.append('monotime>=1.0')
+    install_requires.append('Monotime>=1.0')
 elif sys.version_info.major == 3:
     # Installing Cython==0.20.1 for building gevent
     from setuptools.command.easy_install import main as easy_install
