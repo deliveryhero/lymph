55c9a0cc11fd536d1af53c7eb1328174d099dff9
Johannes Dollinger <johannes.dollinger@deliveryhero.com>
2015-06-25 15:22:51 +0200

    Allow trace_id to be set from lymph emit/request cli

--- lymph/cli/emit.py
+++ lymph/cli/emit.py
@@ -2,11 +2,15 @@ import json
 
 from lymph.client import Client
 from lymph.cli.base import Command
+from lymph.core import trace
 
 
 class EmitCommand(Command):
     """
     Usage: lymph emit <event-type> [<body>] [options]
+
+    Options:
+      --trace-id=<trace_id>        Use the given trace_id.
 
     {COMMON_OPTIONS}
     """
@@ -17,6 +21,6 @@ class EmitCommand(Command):
         event_type = self.args.get('<event-type>')
         body = json.loads(self.args.get('<body>'))
 
+        trace.set_id(self.args.get('--trace-id'))
         client = Client.from_config(self.config)
         client.emit(event_type, body)
-
