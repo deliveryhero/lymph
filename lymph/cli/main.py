7a6fe6c699a33ea92aa98cf477860bfe286e9c5d
Johannes Dollinger <johannes.dollinger@deliveryhero.com>
2015-07-06 15:11:31 +0200

    Allow config file to be specified via LYMPH_CONFIG env var

--- lymph/cli/main.py
+++ lymph/cli/main.py
@@ -18,7 +18,7 @@ def setup_config(args):
     if 'LYMPH_NODE_CONFIG' in os.environ:
         config.load_file(os.environ['LYMPH_NODE_CONFIG'], sections=['container', 'registry', 'event_system', 'plugins', 'dependencies'])
 
-    config_file = args.get('--config') or '.lymph.yml'
+    config_file = args.get('--config') or os.environ.get('LYMPH_CONFIG', '.lymph.yml')
     config.load_file(config_file)
     config.source = config_file
 
