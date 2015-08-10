0eec9e5800823a4e80713395f49e4aa2367489a8
Johannes Dollinger <johannes.dollinger@deliveryhero.com>
2015-08-10 17:10:12 +0200

    Bump version: 0.6.0 → 0.7.0-dev

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ elif sys.version_info.major == 3:
 setup(
     name='lymph',
     url='http://github.com/deliveryhero/lymph/',
-    version='0.6.0',
+    version='0.7.0-dev',
     namespace_packages=['lymph'],
     packages=find_packages(),
     license=u'Apache License (2.0)',
