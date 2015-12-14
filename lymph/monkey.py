1bfd3eac8cf2de3515b85985564da6eccbe8f398
Johannes Dollinger <johannes.dollinger@deliveryhero.com>
2015-12-14 11:34:49 +0100

    Bump version: 0.14.0-dev → 0.14.0

--- lymph/monkey.py
+++ lymph/monkey.py
@@ -18,7 +18,7 @@ patch._initialized = False
 
 def _export():
     import lymph
-    lymph.__version__ = '0.14.0-dev'
+    lymph.__version__ = '0.14.0'
 
     from lymph.exceptions import RpcError, LookupFailure, Timeout
     from lymph.core.decorators import rpc, raw_rpc, event, task
