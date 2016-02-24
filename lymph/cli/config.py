492faa24425c19e42146947fa94bb03338b35911
Drahflow <drahflow@gmx.de>
2016-02-24 16:07:36 +0100

    Don't modify config during logging setup

--- lymph/cli/config.py
+++ lymph/cli/config.py
@@ -2,8 +2,6 @@ from __future__ import print_function
 import yaml
 from lymph.cli.base import Command
 
-import zmq.green.core
-
 
 class ConfigCommand(Command):
     """
@@ -16,10 +14,5 @@ class ConfigCommand(Command):
 
     short_description = 'Prints configuration for inspection'
 
-    @staticmethod
-    def socket_representer(dumper, socket):
-        return dumper.represent_scalar('!socket', '...', style=None)
-
     def run(self):
-        yaml.add_representer(zmq.green.core._Socket, self.socket_representer)
-        print(yaml.dump(self.config.values))
+        print(yaml.safe_dump(self.config.values))
